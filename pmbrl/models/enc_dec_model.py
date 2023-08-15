77cae1aeb7b2a50f4176db92ba3657d0db74a1a1
lzh <2536890272@qq.com>
2023-08-15 19:01:12 +0800

    add fuse fc

--- pmbrl/models/enc_dec_model.py
+++ pmbrl/models/enc_dec_model.py
@@ -16,7 +16,9 @@ class Encoder(nn.Module):
         self.cv4 = nn.Conv2d(32, 64, kernel_size=4, stride=2)  # 64*4
         self.fc1 = nn.Linear(10, 128)
         self.fc2 = nn.Linear(128, 256)
-        self.fc3 = nn.Linear(512, 20)
+        self.fc3 = nn.Linear(512, 256)
+        self.fc4 = nn.Linear(256, 128)
+        self.fc5 = nn.Linear(128, 20)
         self.to(device)
 
     def forward(self, vec, img):
@@ -28,7 +30,9 @@ class Encoder(nn.Module):
         hidden = F.relu(self.fc1(vec))
         hidden = F.relu(self.fc2(hidden))
         hidden = torch.cat([hidden, embedded_obs], dim=1)
-        embedded_obs = self.fc3(hidden)
+        embedded_obs = F.relu(self.fc3(hidden))
+        embedded_obs = F.relu(self.fc4(embedded_obs))
+        embedded_obs = self.fc5(embedded_obs)
         return embedded_obs
 
 
@@ -43,20 +47,24 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
         self.vec_dim = vec_dim
         self.embedded_img_dim = embedded_dim
-        self.fc = nn.Linear(embedded_dim, 256)
+        self.fc = nn.Linear(embedded_dim, 128)
+        self.fc1 = nn.Linear(128, 128)
+        self.fc2 = nn.Linear(128, 256)
         self.dc1 = nn.ConvTranspose2d(256, 64, kernel_size=5, stride=2)
         self.dc2 = nn.ConvTranspose2d(64, 32, kernel_size=5, stride=2)
         self.dc3 = nn.ConvTranspose2d(32, 16, kernel_size=6, stride=2)
         self.dc4 = nn.ConvTranspose2d(16, 3, kernel_size=6, stride=2)
-        self.fc1 = nn.Linear(256, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 10)
         self.to(device)
 
     def forward(self, embedded_obs):
         # TODO 独立的img emb可以切换成混合的
         hidden = F.relu(self.fc(embedded_obs))
-        vec_hidden = F.relu(self.fc1(hidden))
-        vec = self.fc2(vec_hidden)
+        hidden = F.relu(self.fc1(hidden))
+        hidden = F.relu(self.fc2(hidden))
+        vec_hidden = F.relu(self.fc3(hidden))
+        vec = self.fc4(vec_hidden)
         hidden = hidden.view(hidden.size(0), 256, 1, 1)
         hidden = F.relu(self.dc1(hidden))
         hidden = F.relu(self.dc2(hidden))
